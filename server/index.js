ad2fa88e6225a926290e022cd739385f995f2547
Justin <justin.leung.ch@gmail.com>
2021-01-16 14:42:50 -0500

    Update server

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
-app.listen(process.env.PORT || 5000);
+app.listen(PORT, function () {
+    console.log(`App running on port ${PORT}`);
+});
