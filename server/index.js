6d07aebd53899b9f68af49a31e545e0159954d13
Justin <justin.leung.ch@gmail.com>
2021-01-16 14:52:11 -0500

    Update server

--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static('public'));
 
+// For production build on heroku
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
