578f1e1500f76ebe0da0af053d3b43f9bd922658
Justin <justin.leung.ch@gmail.com>
2021-01-17 00:19:40 -0500

    Redirect when refresh page

--- server/index.js
+++ server/index.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
   }
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
